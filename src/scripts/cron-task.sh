0644102f0bb21f3359e6e2b661954ff3db0ad30e
Rodrigo Iloro <rodrigo.iloro@gmail.com>
2025-02-18 12:24:45 -0300

    updating docker configuration.

--- src/scripts/cron-task.sh
+++ src/scripts/cron-task.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-source /app/.env.sh
-cd /app && /usr/local/bin/bun index.js >> /var/log/cron.log 2>&1
+
+cd /app && /usr/local/bin/bun index.js --worker hacker-news >> /var/log/cron.log 2>&1
+cd /app && /usr/local/bin/bun index.js --worker truly-remote >> /var/log/cron.log 2>&1
--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-# Export environment variables to a file that can be sourced
-env | while read -r line; do
-  echo "export $line" >> /app/.env.sh
-done
-chmod +x /app/.env.sh
+
+# fixes issue where environment variables are not available in the crontab
+printenv > /etc/environment
 
 exec "$@"
